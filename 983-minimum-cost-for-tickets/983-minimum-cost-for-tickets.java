dbb0afcc21a42c069f2d9c84770a3b8592990961
Aarav Mishra <aaravmishra619@gmail.com>
2023-03-29 09:31:04 +0530

    Time: 2 ms (42.17%), Space: 42.2 MB (15.86%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,36 +1,38 @@
 class Solution {
-    int n;
     public int mincostTickets(int[] days, int[] costs) {
-        n = days.length; 
+        int n = days.length;
+        int lastDay = days[n-1];
         
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        Set<Integer> st = new HashSet<>(); // to track days only the days of traveling
+        int[] dp = new int[lastDay+1];
         
-        return mincostTickets(0, days, costs, dp);
-    }
-    public int mincostTickets(int i, int[] days, int[] costs, int[] dp) {
-        if(i >= n) return 0;
+        for(int i = 0; i < n; i++)
+            st.add(days[i]);
         
-        if(dp[i] != -1)
-            return dp[i];
+        // dp[i] -> min cost of traveling upto ith day from day 1
         
-        //1 day pass
-        int oneDayPass = costs[0] + mincostTickets(i+1, days, costs, dp);
+        dp[0] = 0; // cost of reaching 0th day
         
-        //7 day pass
-        int j = i;
-        int nextDayToPurchaseTicket = days[i] + 7;
-        while(j < n && days[j] < nextDayToPurchaseTicket) j++;
+        for(int i = 1; i <= lastDay; i++) {
+            if(!st.contains(i)) {// skip this day
+                dp[i] = dp[i-1];
+                continue;
+            } 
+            
+            dp[i] = Integer.MAX_VALUE;
+            
+            // 1 day pass
+            int oneDayPass = costs[0] + dp[Math.max(0, i-1)]; // reaching i from i-1 by adding costs[0] (1day pass cost)
+            
+            // 7 day pass
+            int sevenDayPass = costs[1] + dp[Math.max(0, i-7)]; // reaching i from i-7 by adding costs[1] (7day pass cost)
+            
+            // 30 day pass
+            int thirtyDayPass = costs[2] + dp[Math.max(0, i-30)]; // reaching i from i-30 by adding costs[2] (30day pass cost)
+            
+            dp[i] = Math.min(oneDayPass, Math.min(sevenDayPass, thirtyDayPass));
+        }
         
-        int sevenDaypass = costs[1] + mincostTickets(j, days, costs, dp);
-        
-        //30 day pass
-        j = i;
-        nextDayToPurchaseTicket = days[i] + 30;
-        while(j < n && days[j] < nextDayToPurchaseTicket) j++;
-        
-        int thirtyDayPass = costs[2] + mincostTickets(j, days, costs, dp);
-        
-        return dp[i] = Math.min(oneDayPass, Math.min(sevenDaypass, thirtyDayPass));
+        return dp[lastDay];
     }
 }
